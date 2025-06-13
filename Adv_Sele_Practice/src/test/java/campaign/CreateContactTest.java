93a8420ca484fe9fc31ced650ea8042fb34a453b
Swetha05-5 <swethanice95@gmail.com>
2025-06-13 08:15:04 +0530

    Commited2

--- Adv_Sele_Practice/src/test/java/campaign/CreateContactTest.java
+++ Adv_Sele_Practice/src/test/java/campaign/CreateContactTest.java
@@ -33,8 +33,5 @@ public class CreateContactTest extends BaseClass {
 		ctt.getContactNameTF().sendKeys(excel.readDataFromExcelFile("Contact", 1, 6));
 		ctt.getMobileTF().sendKeys(excel.readNumericDataFromExcelFile("Contact", 1, 7));
 		ctt.getCreateContactSubmitBtn();
-	
-		
-		
 		
 	}}
