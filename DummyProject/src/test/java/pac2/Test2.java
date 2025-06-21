bd078bf0f09cbba87dfd2933834ac890b3e45803
Swetha05-5 <swethanice95@gmail.com>
2025-06-21 10:57:44 +0530

    Commit

---
+++ DummyProject/src/test/java/pac2/Test2.java
@@ -0,0 +1,16 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test(groups="Regression")
+	public void test() {
+		System.out.println("test");
+	}
+	@Test(groups="Regression")
+	public void test1() {
+		System.out.println("test1");
+	}
+
+}
